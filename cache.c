f4f07d3b135ef9e77642603260b5f4a7dc343ab1
Rajarshi Das <rajarshids609@gmail.com>
2024-07-12 23:45:03 -0500

    Update cache.c

--- cache.c
+++ cache.c
@@ -1,59 +1,67 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <windows.h>
 
-#define MIN_ELEMENT 256  // Since int is 4 byte, so the minimum number of element is 256
-#define SIZE_ORDER 17  // Array size is from 1KB to 64MB
+#define MIN_ELEMENT 256  // Minimum number of elements (1KB for int array)
+#define SIZE_ORDER 17    // Array size ranges from 1KB to 64MB
 #define INIT_STRIDE 8
-#define AVERAGE 10
-unsigned int numOperation = 1 << 25;
+#define AVERAGE 10       // Number of iterations for averaging
+#define NUM_OPERATIONS (1 << 25)
 
-int MeasureTime(int n, int s);
-int CalculateOneRound(int n, int s);
+int measureTime(int arraySize, int stride);
+int calculateOneRound(int arraySize, int stride);
 
-int CalculateOneRound(int n, int s) {
-    int i;
-    unsigned long long interval;
-    LARGE_INTEGER frequency, begin, end;
-    int *arr = (int *)malloc(sizeof(int) * n);
-    for (i = 0; i < n; i++) {
-        arr[i] = 0;
+int calculateOneRound(int arraySize, int stride) {
+    struct timespec start, end;
+    int *array = (int *)malloc(sizeof(int) * arraySize);
+    if (array == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
     }
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&begin);
-    for (i = 0; i < numOperation; i++) {
-        arr[(i * s) & (n - 1)] += 1;
+
+    memset(array, 0, sizeof(int) * arraySize);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (unsigned int i = 0; i < NUM_OPERATIONS; i++) {
+        array[(i * stride) & (arraySize - 1)] += 1;
     }
-    QueryPerformanceCounter(&end);
-    interval = (end.QuadPart - begin.QuadPart) * 1000000 / frequency.QuadPart;
-    free(arr);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    unsigned long long interval = (end.tv_sec - start.tv_sec) * 1000000LL + 
+                                  (end.tv_nsec - start.tv_nsec) / 1000LL;
+
+    free(array);
     return (int)interval;
 }
 
-int MeasureTime(int n, int s) {
-    int i, cur = 0, total = 0;
-    for (i = 0; i < AVERAGE; i++) {
-        cur = CalculateOneRound(n, s);
-        total += cur / 10;
+int measureTime(int arraySize, int stride) {
+    int total = 0;
+    for (int i = 0; i < AVERAGE; i++) {
+        total += calculateOneRound(arraySize, stride);
     }
-    return total;
+    return total / AVERAGE;
 }
 
 int main() {
-    int i, cost, j;
-    FILE *fp1 = fopen("Data.txt", "w");
-    for (j = 0; j < 5; j++) {
+    FILE *file = fopen("Data.txt", "w");
+    if (file == NULL) {
+        fprintf(stderr, "Failed to open file\n");
+        return EXIT_FAILURE;
+    }
+
+    for (int j = 0; j < 5; j++) {
         int stride = INIT_STRIDE << j;
-        for (i = 0; i < SIZE_ORDER; i++) {
+        for (int i = 0; i < SIZE_ORDER; i++) {
             int arraySize = MIN_ELEMENT * (1 << i);
-            cost = MeasureTime(arraySize, stride);
-            fprintf(fp1, "%d,%d,%d\n", i, stride, cost);
+            int cost = measureTime(arraySize, stride);
+            fprintf(file, "%d,%d,%d\n", i, stride, cost);
         }
     }
-    fclose(fp1);
-    printf("Successfully ouput the data!\n");
+
+    fclose(file);
+    printf("Successfully output the data!\n");
     return 0;
 }
